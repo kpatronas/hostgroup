be53eef6c8cb15f64162a981b4a36e549540cb22
Konstantinos Patronas <kpatronas@gmail.com>
2022-01-24 13:09:07 +0200

    Delete hostenv.sh

--- hostenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]
-then
-      echo "No environment is given."
-      exit 1
-fi
-
-ansible -m debug -a 'msg={{group_names}}' $1 | tr -d " " | tr -s '\n' ' ' | tr -s '}' '\n'  | tr -d " " | tr -s "|" " " | tr -s "[]" " " | cut -d " " -f1,3 | tr -d '"'
